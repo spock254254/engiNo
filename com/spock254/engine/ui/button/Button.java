a82c3564bc96eab654e11ccdeffcdd4d26a5c7bc
Aleksandr Levitan <aleksandrlevitan254z@gmail.com>
2018-05-28 16:41:11 +0300

    added isHover and isBtnClicked

--- com/spock254/engine/ui/button/Button.java
+++ com/spock254/engine/ui/button/Button.java
@@ -3,38 +3,72 @@ package com.spock254.engine.ui.button;
 import com.spock254.engine.GameContainer;
 import com.spock254.engine.interfaces.draw.IDrawingShape;
 import com.spock254.engine.interfaces.draw.IDrawingText;
+import com.spock254.engine.ui.UIColor;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 
 public class Button extends BasicButton {
 
-    private int hoverColor;
-    private int initColor = super.getButtonColor();
 
-    public Button(GameContainer gc, IDrawingShape shape, IDrawingText text, int buttonOffX, int buttonOffY, int buttonW, int buttonH, int buttonColor, String buttonText, int textOffX, int textOffY, int textColor,
-                  int hoverColor) {
-        super(gc, shape, text, buttonOffX, buttonOffY, buttonW, buttonH, buttonColor, buttonText, textOffX, textOffY, textColor);
-        this.hoverColor = hoverColor;
+    private UIColor textColor;
+    private UIColor btnColor;
+    private int initBtnColor;
+    private int initTextColor;
+
+
+
+    public Button(GameContainer gc, IDrawingShape shape, IDrawingText text, int buttonOffX, int buttonOffY, int buttonW, int buttonH, String buttonText, int textOffX, int textOffY,
+                  UIColor textColor,UIColor btnColor) {
+        super(gc, shape, text, buttonOffX, buttonOffY, buttonW, buttonH, btnColor.getBaseColor(), buttonText, textOffX, textOffY, textColor.getBaseColor());
+        this.textColor = textColor;
+        this.btnColor = btnColor;
+        initBtnColor = btnColor.getBaseColor();
+        initTextColor = textColor.getBaseColor();
     }
 
-    public Button(GameContainer gc, IDrawingShape shape, IDrawingText text, int buttonOffX, int buttonOffY, int buttonW, int buttonH, int buttonColor, String buttonText, int textColor,
-                  int hoverColor) {
-        super(gc, shape, text, buttonOffX, buttonOffY, buttonW, buttonH, buttonColor, buttonText, textColor);
-        this.hoverColor = hoverColor;
+    public Button(GameContainer gc, IDrawingShape shape, IDrawingText text, int buttonOffX, int buttonOffY, int buttonW, int buttonH, String buttonText,
+                  UIColor textColor,UIColor btnColor) {
+        super(gc, shape, text, buttonOffX, buttonOffY, buttonW, buttonH, btnColor.getBaseColor(), buttonText, textColor.getBaseColor());
+        this.textColor = textColor;
+        this.btnColor = btnColor;
+        initBtnColor = btnColor.getBaseColor();
+        initTextColor = textColor.getBaseColor();
     }
+
 
     @Override
     public void drawButton() {
         super.drawButton();
 
-        if(super.getButtonOffX() <= super.getGameContainer().getInput().getMouseX()
+        //if(this.isBtnCliced(0)){
+        //    System.out.println("Click!");
+        //    super.setButtonColor(this.btnColor.getActiveColor());
+        //    super.setTextColor(this.textColor.getActiveColor());
+
+        //}
+        if(this.isHover()){
+            super.setButtonColor(this.btnColor.getHoverColor());
+            super.setTextColor(this.textColor.getHoverColor());
+            if(isBtnCliced(0)){
+                System.out.println("Click");
+                super.setButtonColor(this.btnColor.getActiveColor());
+                super.setTextColor(this.textColor.getActiveColor());
+                // TODO add deligate 
+            }
+        }else {
+            super.setButtonColor(initBtnColor);
+            super.setTextColor(initTextColor);
+        }
+    }
+    public boolean isHover(){
+        return (super.getButtonOffX() <= super.getGameContainer().getInput().getMouseX()
                 && (super.getButtonOffX()+super.getButtonW()) >= super.getGameContainer().getInput().getMouseX()
                 && super.getButtonOffY() <= super.getGameContainer().getInput().getMouseY()
-                && (super.getButtonOffY() + super.getButtonH() >= super.getGameContainer().getInput().getMouseY())){
-            super.setButtonColor(this.hoverColor);
-        }else {
-            super.setButtonColor(initColor);
-        }
-
-
-
+                && (super.getButtonOffY() + super.getButtonH() >= super.getGameContainer().getInput().getMouseY()));
     }
+    public boolean isBtnCliced(int keyCode){ // TODO : make set up for key event
+        return (super.getGameContainer().getInput().isButton(MouseEvent.BUTTON1));
+    }
+
 }
--- com/spock254/game/GameManager.java
+++ com/spock254/game/GameManager.java
@@ -6,6 +6,7 @@ import com.spock254.engine.Renderer;
 import com.spock254.engine.draw.RectFilledDraw;
 import com.spock254.engine.draw.TextDraw;
 import com.spock254.engine.gfx.Font;
+import com.spock254.engine.ui.UIColor;
 import com.spock254.engine.ui.button.BasicButton;
 import com.spock254.engine.ui.button.Button;
 
@@ -24,9 +25,13 @@ public class GameManager extends AbstractGame {
           //                      10,10,40,20,0xffE6118A,
             //                    "Start",19,17,0xff2B0B1D);
 
+        //button2 = new Button(gameContainer,new RectFilledDraw(gameContainer),new TextDraw(gameContainer,new Font("/res/standart.png")),
+        //        11,41,20,15,0xffE6118A,
+        //        "Start",0xff2B0B1D,0xff9FFFA6);
         button2 = new Button(gameContainer,new RectFilledDraw(gameContainer),new TextDraw(gameContainer,new Font("/res/standart.png")),
-                11,41,20,15,0xffE6118A,
-                "Start",0xff2B0B1D,0xff9FFFA6);
+                10,20,50,30,"Start",
+                new UIColor(0xff524C3D,0xff7C4E40,0xffF65828,0xffCAB4AE),
+                new UIColor(0xff6AF96F,0xffA4F7A6,0xffF5DDA6,0xffD8CEB5));
 
     }
 
